e502253886fe0bc46e2c5bad02beeed77eb62528
Duarte43000 <duarte43000@gmail.com>
2022-10-06 12:51:23 +0100

    Enviar

--- Android/App2/app/src/main/java/com/example/app2/MainActivity3.java
+++ Android/App2/app/src/main/java/com/example/app2/MainActivity3.java
@@ -2,15 +2,13 @@ package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
     }
 }

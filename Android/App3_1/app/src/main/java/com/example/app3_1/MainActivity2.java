d5cad0f2ba5854c7a9ef9ef4b252b832ad3254ba
Duarte43000 <duarte43000@gmail.com>
2022-10-13 12:40:37 +0100

    Mostra imagens em grande

--- Android/App3_1/app/src/main/java/com/example/app3_1/MainActivity2.java
+++ Android/App3_1/app/src/main/java/com/example/app3_1/MainActivity2.java
@@ -3,6 +3,8 @@ package com.example.app3_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void send4(View view) {
+        ImageView iv=findViewById(R.id.imageView);
+                iv.setImageResource(R.mipmap.mickey4);
+    }
+    public void send5(View view) {
+        ImageView iv=findViewById(R.id.imageView);
+        iv.setImageResource(R.mipmap.mickey5);
+    }
+    public void send6(View view) {
+        ImageView iv=findViewById(R.id.imageView);
+        iv.setImageResource(R.mipmap.mickey6);
+    }
+    public void send7(View view) {
+        ImageView iv=findViewById(R.id.imageView);
+        iv.setImageResource(R.mipmap.mickey7);
     }
 }

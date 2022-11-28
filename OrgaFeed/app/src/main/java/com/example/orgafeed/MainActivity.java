50bcc04b2c04a1b8d65ac6d8af5f76856bccb943
Duarte43000 <duarte43000@gmail.com>
2022-11-28 10:25:11 +0000

    segundo ecra

--- OrgaFeed/app/src/main/java/com/example/orgafeed/MainActivity.java
+++ OrgaFeed/app/src/main/java/com/example/orgafeed/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void send(View view) {
         Intent intent=new Intent(this,MainActivity2.class);
         startActivity(intent);

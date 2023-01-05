6f9239fbbaf6f2423ea27308b74a4298b3e41c83
Duarte43000 <duarte43000@gmail.com>
2023-01-05 11:31:24 +0000

    .

--- OrgaFeed/app/src/main/java/com/example/orgafeed/MainActivity2.java
+++ OrgaFeed/app/src/main/java/com/example/orgafeed/MainActivity2.java
@@ -1,5 +1,6 @@
 package com.example.orgafeed;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -61,5 +62,13 @@ public class MainActivity2 extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void Send(View view) {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,view.getId());
+        System.out.println("estou aqui");
     }
 }

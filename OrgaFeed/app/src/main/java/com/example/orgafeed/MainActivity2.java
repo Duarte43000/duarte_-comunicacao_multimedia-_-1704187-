f68246d51c1118163ac70fb426b2d562469c3ac9
Duarte43000 <duarte43000@gmail.com>
2023-01-10 18:57:46 +0000

    update

--- OrgaFeed/app/src/main/java/com/example/orgafeed/MainActivity2.java
+++ OrgaFeed/app/src/main/java/com/example/orgafeed/MainActivity2.java
@@ -1,26 +1,32 @@
 package com.example.orgafeed;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.orgafeed.databinding.ActivityMain2Binding;
 
 public class MainActivity2 extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMain2Binding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +35,31 @@ public class MainActivity2 extends AppCompatActivity {
         binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 System.out.println("ola mundo");
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"),0);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"),123);
 
             }
+
+
         });
+
+
+
+
+
+
+
+
+
+
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
